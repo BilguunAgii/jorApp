2db4f6e35031516cf3906604543b710400a7e474
BilguunAgii <100501368+BilguunAgii@users.noreply.github.com>
2024-01-27 18:19:08 +0800

    Хоолны жорын View хэсгийг бичих, жорыг дэлгэц дээр гаргаж ирэх

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@ require("@babel/polyfill");
 import Search from "./model/Search";
 import {elements, renderLoader,clearLoader} from './view/base';
 import * as searchView from './view/searchView';
-
+import Recipe from "./model/Recipe";
+import {renderRecipe, clearRecipe, highlightSelectedRecipe} from './view/recipeView';
 const state = {}
 
 const controlSearch = async () => {
@@ -12,7 +13,7 @@ const controlSearch = async () => {
         state.search = new Search(query);
         
         searchView.clearSearchQuery();
-        searchView.clearSearchList();
+        searchView.clearSearchResult();
 
         renderLoader(elements.searchResultDiv)
         await state.search.doSearch();
@@ -21,7 +22,7 @@ const controlSearch = async () => {
         if (state.search.result === undefined){
             alert('Хайлт илэрцгүй');
         }
-        else{searchView.renderRecipes(state.search. result);
+        else{searchView.renderRecipes(state.search. result, 1, 10);
         }
     }
 }
@@ -29,3 +30,37 @@ elements.searchForm.addEventListener("submit", e => {
     e.preventDefault();
     controlSearch();
 });
+
+elements.pageButtons.addEventListener("click", e => {
+    const btn = e.target.closest(".btn-inline");
+    if (btn) {
+      const gotoPageNumber = parseInt(btn.dataset.goto, 10);
+      searchView.clearSearchResult();
+      searchView.renderRecipes(state.search.result, gotoPageNumber);
+    }
+  });
+  
+  const r = new Recipe(47746); 
+  r.getRecipe();
+
+  //Joriin controller
+const controlRecipe = async () => {
+    //Urlaas ID salgaj awna
+    const id = window.location.hash.replace("#", '');
+    //joriin model uusgej ogno
+    state.recipe = new Recipe(id);
+    //UI delgetsiig beltgene
+    clearRecipe();
+    renderLoader(elements.recipeDev);
+    highlightSelectedRecipe(id);
+    
+    //Joroo tataj awchirna
+    await state.recipe.getRecipe();
+    clearLoader();
+    state.recipe.calcTime();
+    state.recipe.calcHuniiToo();
+    //Joroo delgetsend gargah
+    renderRecipe(state.recipe);
+} 
+window.addEventListener("hashchange", controlRecipe);
+window.addEventListener("load", controlRecipe);
--- src/js/view/base.js
+++ src/js/view/base.js
@@ -1,23 +1,31 @@
 export const elements = {
-    searchForm: document.querySelector('.search'),
-    searchInput: document.querySelector('.search__field'),
-    searchResultList: document.querySelector('.results__list'),
-    searchResultDiv: document.querySelector('.results')
-};
-export const elementString = {
-    loader: 'loader'
-};
-
-export const clearLoader = () =>{
-    const loader = document.querySelector(`.${elementString.loader}`);
-    if(loader) loader.parentElement.removeChild(loader);    
-};
-
-export const renderLoader = parent => {
-    const loader = ` <div class="${elementString.loader}">
-        <svg>
-            <use href="img/icons.svg#icon-cw"</use>
-        </svg>
-    </div>`;    
-    parent.insertAdjacentHTML('afterbegin', loader);
-}
+    searchForm: document.querySelector(".search"),
+    searchInput: document.querySelector(".search__field"),
+    searchResultDiv: document.querySelector(".results"),
+    searchResultList: document.querySelector(".results__list"),
+    pageButtons: document.querySelector(".results__pages"),
+    recipeDev: document.querySelector(".recipe"),
+  };
+  
+  export const elementStrings = {
+    loader: "loader"
+  };
+  
+  export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+  
+    if (loader) loader.parentElement.removeChild(loader);
+  };
+  
+  export const renderLoader = parent => {
+    const loader = `
+          <div class="${elementStrings.loader}">
+              <svg>
+                  <use href="img/icons.svg#icon-cw"</use>
+              </svg>
+          </div>
+      `;
+  
+    parent.insertAdjacentHTML("afterbegin", loader);
+  };
+  
